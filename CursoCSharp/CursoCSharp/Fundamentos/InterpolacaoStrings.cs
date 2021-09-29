13e576661b815007181a5305f66c5900fa41ddc5
Cleberton <clebertonfgc@gmail.com>
2021-09-29 08:34:50 -0300

    Finaliando aulaInterolação de strings

--- CursoCSharp/CursoCSharp/Fundamentos/InterpolacaoStrings.cs
+++ CursoCSharp/CursoCSharp/Fundamentos/InterpolacaoStrings.cs
@@ -10,7 +10,18 @@ namespace CursoCSharp.Fundamentos
     {
         public static void Executar()
         {
+            string nome = "Notbook Gamer";
+            string marca = "Dell";
+            double preco = 5800.00;
 
+            // Primeira forma de interpolação
+            Console.WriteLine("O " + nome + " da marca " + marca + " custa " + preco + ".");
+
+            // Segunda forma de interpolação (mais eficiente)
+            Console.WriteLine($"A marca de computadores {marca} é muito Boa!");
+
+            // Terceira forma de interpolação (mais eficiente)
+            Console.WriteLine("O {0} da marca {1} custa {2}", nome, marca, preco);
         }
     }
 }
