33a1cc8e724ec29e87717144c9d05a38fdf20f47
Cleberton <clebertonfgc@gmail.com>
2021-09-28 21:39:32 -0300

    Aula de comentarios

---
+++ CursoCSharp/CursoCSharp/Fundamentos/Comentarios.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.Fundamentos
+{
+    /// <summary>
+    ///  XML comments
+    /// </summary>
+    class Comentarios
+    {
+        public static void Executar()
+        {
+            // Cuidado com os comentaários desnecessários
+            Console.WriteLine("Código claro é sempre melhor!");
+
+            /*
+                Comentários de  multiplas linhas
+            */
+
+            // Comentários de uma linha
+        }
+    }
+}
