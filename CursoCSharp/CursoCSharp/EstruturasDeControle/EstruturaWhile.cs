a749fd9592960ef50ee3cc15b7556492760b5cc8
Cleberton <clebertonfgc@gmail.com>
2021-09-30 09:11:36 -0300

    Estrutura repetição While

---
+++ CursoCSharp/CursoCSharp/EstruturasDeControle/EstruturaWhile.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CursoCSharp.EstruturasDeControle
+{
+    class EstruturaWhile
+    {
+        public static void Executar()
+        {
+      
+            bool continuar = true;
+
+            // Enquanto a expressão for verdadeira execute o bloco
+
+            while (continuar)
+            {
+                Console.WriteLine("Digite um numero entre 0 e 5");
+                int valorDigitado;
+                var valor = Console.ReadLine();
+                int.TryParse(valor, out valorDigitado);
+
+                if (valorDigitado >=0 && valorDigitado <= 5) 
+                {
+                    Console.WriteLine($"Obrigado! o numero digitado foi {valorDigitado}");
+                } else
+                {
+                    Console.WriteLine("Opps! o numero deve estar entre 0 e 5!");
+                }
+
+                Console.WriteLine("Deseja continuar S / N?");
+                var continuarLoop = Console.ReadLine().ToLower();
+
+                if (continuarLoop == "n")
+                {
+                    continuar = false;
+                }
+            }
+
+        }
+    }
+}
