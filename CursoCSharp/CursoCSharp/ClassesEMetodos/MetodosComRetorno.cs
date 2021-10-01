22d5ed48c655ae6cdd3adabd34a6aaacf6896e2b
Cleberton <clebertonfgc@gmail.com>
2021-10-01 11:57:52 -0300

    Metodos retornando a propria instacia da classe, e metodos normais

--- CursoCSharp/CursoCSharp/ClassesEMetodos/MetodosComRetorno.cs
+++ CursoCSharp/CursoCSharp/ClassesEMetodos/MetodosComRetorno.cs
@@ -25,12 +25,50 @@ namespace CursoCSharp.ClassesEMetodos
                 return a * b;
             }
         }
+
+        class CalculadoraCadeia
+        {
+           int memoria; // Variavel interna do método
+
+            public CalculadoraCadeia Somar (int a)
+            {
+                memoria += a;
+                return this; // Retornando o objeto (retorna a prorpria CalculadoraCadeia Somar )
+                // Pode-se quando instânciar utilizar a notação ponto
+            }
+
+            public CalculadoraCadeia multiplicar(int a)
+            {
+                memoria *= a;
+                return this;
+            }
+
+            public CalculadoraCadeia Imprimir()
+            {
+                Console.WriteLine(memoria);
+                return this;
+            }
+
+            public CalculadoraCadeia Limpar()
+            {
+                memoria = 0;
+                return this;
+            }
+        }
+
         public static void Executar()
         {
+            // Calculadora Normal
             var calculadora = new Calculadora();
             Console.WriteLine(calculadora.Somar(5, 15));
             Console.WriteLine(calculadora.Subtrair(5, 15));
             Console.WriteLine(calculadora.Multiplicar(5, 15));
+
+            // Calculadora Cadeia
+
+            var calculadoraCadeia = new CalculadoraCadeia();
+            // Desta forma podemos usar notação ponto
+            calculadoraCadeia.Somar(5).Somar(5).multiplicar(3).Imprimir();
         }
     }
 }
