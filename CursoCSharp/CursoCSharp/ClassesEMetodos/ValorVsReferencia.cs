67fc17471100f6c40780223a843e74c634c74060
Cleberton <clebertonfgc@gmail.com>
2021-10-20 18:03:00 -0300

    Add cometarios

--- CursoCSharp/CursoCSharp/ClassesEMetodos/ValorVsReferencia.cs
+++ CursoCSharp/CursoCSharp/ClassesEMetodos/ValorVsReferencia.cs
@@ -15,15 +15,21 @@ namespace CursoCSharp.ClassesEMetodos
         }
         public static void Executar()
         {
+            // Passagem por valor (Tanto o nuemro como a copia são independentes)
+            // Struct e por valor
             int numero = 3;
             int copiaNumero = numero;
 
             Console.WriteLine($"Numero: {numero} / Copia: {copiaNumero}");
 
+            // Se alteramos o numero aqui, ele não afeta a copia (pois a passagem é feita por valor)
             numero++;
 
+            // Passagem por valor
             Console.WriteLine($"Numero ++: {numero}");
 
+            // Ja a classe e por referencia, então caso a variavel seja alterada suas copias tambem serão
+            // pois a copia é somente uam referencia na memoria
             Dependente dep = new Dependente { Nome = "Cleber", Idade = 28 };
 
             Dependente copiaDep = dep;
